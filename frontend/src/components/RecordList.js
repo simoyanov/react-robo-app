95b912a1e7e6e01eee88bdf6ebb5a4b441e237fd
Maria Simoianova <simoianova.maria@heliogames.com>
2023-09-22 11:58:22 +0300

    add mui and add sorting to recordlist

--- frontend/src/components/RecordList.js
+++ frontend/src/components/RecordList.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import DeleteConfirmationModal from "./DeleteConfirmationModal"; // Импорт компонента DeleteConfirmationModal
+import DeleteConfirmationModal from "./DeleteConfirmationModal";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  IconButton,
+} from "@mui/material";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { Delete as DeleteIcon, Edit as EditIcon } from "@mui/icons-material"; // Импорт иконки Material-UI
 
 function RecordList() {
   const [records, setRecords] = useState([]);
@@ -13,6 +23,7 @@ function RecordList() {
       .get("/api/records")
       .then((response) => {
         setRecords(response.data);
+        console.log(response.data);
       })
       .catch((error) => {
         console.error("Ошибка при получении данных:", error);
@@ -50,42 +61,86 @@ function RecordList() {
     setDeleteModalVisible(false);
   };
 
+  const columns = [
+    { field: "firstName", headerName: "First name", width: 130 },
+  ];
+
   return (
     <div>
       <h2>Список записей</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Имя</th>
-            <th>Номер телефона</th>
-            <th>Email</th>
-            <th>Страна</th>
-            <th>Штат</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
+      {/* <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Имя</TableCell>
+            <TableCell>Номер телефона</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Страна</TableCell>
+            <TableCell>Штат</TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {records.map((record) => (
-            <tr key={record.id}>
-              <td>{record.id}</td>
-              <td>{record.name}</td>
-              <td>{record.phone}</td>
-              <td>{record.email}</td>
-              <td>{record.country}</td>
-              <td>{record.state}</td>
-              <td>
-                <Link to={`/edit/${record.id}`}>Редактировать</Link>
-              </td>
-              <td>
-                <button onClick={() => handleDelete(record)}>Удалить</button>
-              </td>
-            </tr>
+            <TableRow key={record.id}>
+              <TableCell>{record.name}</TableCell>
+              <TableCell>{record.phone}</TableCell>
+              <TableCell>{record.email}</TableCell>
+              <TableCell>{record.country}</TableCell>
+              <TableCell>{record.state}</TableCell>
+              <TableCell>
+                <Link to={`/edit/${record.id}`}>
+                  <IconButton edge="end" aria-label="edit">
+                    <EditIcon />
+                  </IconButton>
+                </Link>
+              </TableCell>
+              <TableCell>
+                <IconButton
+                  edge="end"
+                  aria-label="delete"
+                  onClick={() => handleDelete(record)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
           ))}
-        </tbody>
-      </table>
-      {/* Модальное окно подтверждения удаления */}
+        </TableBody>
+      </Table> */}
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={records}
+          columns={[
+            { field: "name", headerName: "Имя", width: 200 },
+            { field: "phone", headerName: "Номер телефона", width: 200 },
+            { field: "email", headerName: "Email", width: 250 },
+            { field: "country", headerName: "Страна", width: 250 },
+            { field: "state", headerName: "Штат", width: 250 },
+            {
+              field: "actions",
+              headerName: "Действия",
+              width: 150,
+              renderCell: (params) => (
+                <div>
+                  <Link to={`/edit/${params.row.id}`}>
+                    <IconButton color="primary" aria-label="edit">
+                      <EditIcon />
+                    </IconButton>
+                  </Link>
+                  <IconButton
+                    color="secondary"
+                    aria-label="delete"
+                    onClick={() => handleDelete(params.row)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </div>
+              ),
+            },
+          ]}
+        />
+      </div>
       {deleteModalVisible && (
         <DeleteConfirmationModal
           message={`Вы действительно хотите удалить запись "${recordToDelete.name}"?`}
